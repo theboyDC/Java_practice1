1798882c5fb055dbe627d091341b8b7b13c3e66c
wambeswgc025 <wambeswgc025@student.wethinkcode.co.za>
2026-05-05 13:21:13 +0200

    just a few practices

---
+++ src/main/java/com/washie/Main.java
@@ -0,0 +1,22 @@
+package com.washie;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+// list
+//       int[] numbers = {40,25,15,35,20,5,30,10};
+//       Arrays.sort(numbers);
+//        System.out.println(Arrays.toString(numbers));
+
+
+
+
+//MUTLIDIMENSIONAL(2 and 3D) ARRAYS
+//        int [][] numbers = new int[3][3];
+//        numbers[0][0] = 1;
+//        System.out.println(Arrays.deepToString(numbers));
+    }
+
+
+}
