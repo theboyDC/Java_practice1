9aca33ab9114a1db2d6e665cbb537682e8ff4dd9
Washington Mbeleki <wambeswgc025@student.wethinkcode.co.za>
2026-05-17 15:30:32 +0200

    kata7

---
+++ src/main/java/com/washie/CharProblem.java
@@ -0,0 +1,10 @@
+package com.washie;
+
+public class CharProblem {
+    public static int howOld(final String herOld) {
+        char sAge = herOld.charAt(0);
+        return Integer.parseInt(String.valueOf(sAge));
+        //your code here, return correct age as int ; )
+
+    }
+}
--- src/main/java/com/washie/Kata7.java
+++ src/main/java/com/washie/Kata7.java
@@ -1,12 +1,6 @@
 package com.washie;
 
-class Kata7 {
-    static Integer find(final int[] array) {
-        for (int i = 1; i < array.length; i++){
-            if (array[i] != array[i- 1] + 1){
-                return array[i];
-            }
         }
-        return null;
+        }
     }
 }
